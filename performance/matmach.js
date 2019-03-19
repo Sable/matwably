73cbb0589fc05f39152e05cfb2fc0a61016c7f15
David <davidfherrerar@gmail.com>
2019-03-19 15:49:57 -0400

    Deleting extra unnecessary performance folder

--- performance/matmach.js
+++
@@ -1,6 +0,0 @@
-async function getModule() {
-    let wi = await WebAssembly.instantiate(fs.readFileSync("./builtins.wasm"),libjs);
-    let mod = wi.instance.exports;
-    return mod;
-}
-module.exports.instantiateWasm = getModule;
